82465387a108bd46246b4d455ea2c4ff36bb7fc1
0011sinon <154111002+0011sinon@users.noreply.github.com>
2023-12-25 10:49:46 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://0011sinon.github.io/frog11029050/model.json';
 
 
 function preload() {
